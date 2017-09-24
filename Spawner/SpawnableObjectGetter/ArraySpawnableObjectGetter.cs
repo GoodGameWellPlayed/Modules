98c45504b58277388bb22478c949a2b869213af4
ivan zhirnov <ivanzhirnov170991@outlook.com>
2017-09-24 20:27:57 +0300

    Spawner is working

--- Spawner/SpawnableObjectGetter/ArraySpawnableObjectGetter.cs
+++ Spawner/SpawnableObjectGetter/ArraySpawnableObjectGetter.cs
@@ -1,23 +1,6 @@
 ﻿using UnityEngine;
 
-public abstract class ArraySpawnableObjectGetter<T> : MonoBehaviour, ISpawnableObjectGetter<int, T>
-    where T : MonoBehaviour, ISpawnableObject
-{
-    [SerializeField] private string _name;
-    [SerializeField] private T[] _spawnableObjects;
-
-    public T GetRandomSpawnableObject()
-    {
-        return _spawnableObjects[Random.Range(0, _spawnableObjects.Length)];
-    }
-
-    public T GetSpawnableObject(int objectIdentity)
-    {
-        return _spawnableObjects[objectIdentity];
-    }
-}
-
-public class ArraySpawnableObjectGetter : ISpawnableObjectGetter<int>
+public class ArraySpawnableObjectGetter : MonoBehaviour, ISpawnableObjectGetter<int>
 {
     [SerializeField] private string _name;
     [SerializeField] private SpawnableObjectWrapper[] _spawnableObjects;
