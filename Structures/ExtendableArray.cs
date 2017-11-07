ac0ee0ce898c6a441b5124160738326e349e4cac
petr_pes <ivanzhirnov170991@outlook.com>
2017-11-08 01:30:20 +0300

    Добавил структуры

---
+++ Structures/ExtendableArray.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Components.Structures.Arrays
+{
+    /// <summary>
+    /// Массив с фиксированной длиной и сложной операцией расширения.
+    /// Быстрая операция очистки.
+    /// </summary>
+    /// <typeparam name="T">Тип элементов</typeparam>
+    public class ExtendableArray<T> : IEnumerable<T>, IDisposable
+    {
+        //TODO быстрая очистка + IList
+        private ExtendableArrayEnumerator _enumerator;
+        private bool _setDefaultOnDispose;
+
+        public ExtendableArray(bool setDefaultOnDispose = false)
+        {
+            _enumerator = new ExtendableArrayEnumerator();
+            _setDefaultOnDispose = setDefaultOnDispose;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _enumerator;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _enumerator;
+        }
+
+        public void Dispose()
+        {
+            _enumerator.Dispose(_setDefaultOnDispose);
+        }
+
+        public void Add(T obj)
+        {
+            _enumerator.Add(obj);
+        }
+
+        private class ExtendableArrayEnumerator : IEnumerator<T>
+        {
+            private T[] array;
+            private int CountMax { get; set; }
+            private const int CountMin = 10;
+
+            private int CurrentCountEnumerator = -1;
+            private int CurrentCountAdded = 0;
+
+            public ExtendableArrayEnumerator()
+            {
+                array = new T[CountMin];
+            }
+
+            public T Current
+            {
+                get
+                {
+                    return array[CurrentCountEnumerator];
+                }
+            }
+
+            object IEnumerator.Current
+            {
+                get
+                {
+                    return Current as object;
+                }
+            }
+
+            public void Dispose(bool setDefaultOnDispose)
+            {
+                CurrentCountAdded = 0;
+
+                if (setDefaultOnDispose)
+                {
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        array[i] = default(T);
+                    }
+                }
+            }
+
+            public bool MoveNext()
+            {
+                CurrentCountEnumerator++;
+
+                if (CurrentCountEnumerator >= CurrentCountAdded || CurrentCountEnumerator >= array.Length)
+                {
+                    Reset();
+                    return false;
+                }
+
+                return true;
+            }
+
+            public void Reset()
+            {
+                CurrentCountEnumerator = -1;
+            }
+
+            public void Add(T obj)
+            {
+                CurrentCountAdded++;
+
+                if (array.Length < CurrentCountAdded)
+                {
+                    ExtendArray();
+                }
+
+                array[CurrentCountAdded - 1] = obj;
+            }
+
+            private void ExtendArray()
+            {
+                T[] arrayTMP = new T[array.Length + CountMin];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    arrayTMP[i] = array[i];
+                }
+
+                array = arrayTMP;
+            }
+
+            public void Dispose()
+            {
+                Dispose(false);
+            }
+        }
+    }
+}
+
---
+++ Structures/LimitedSizeArray.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Components.Structures.Arrays
+{
+    /// <summary>
+    /// Содержит массив постоянного размера. Если при добавлении элемента количество 
+    /// элементов выходит за пределы массива, первый элемент массива перезаписывается
+    /// последним добавленым.
+    /// Характеризуется быстрой очисткой, добавлением и обращением к определенному элементу массива.
+    /// Следует заметить, что при кол-ве элементов > изначальному размеру массива
+    /// доступ к первым [Кол - во добавленых элементов - Длина массива] невозможен.
+    /// Хорошо подходит для подсчета среднего арифметоческого с постоянным добавлением элементов
+    /// </summary>
+    /// <typeparam name="T">Тип элементов</typeparam>
+    public class LimitedSizeArray<T> : IList<T>, IEnumerator<T>
+    {
+        private T[] _array;
+        private int _firstArrayIndex;
+
+        private int _firstIndex;
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly { get { return false; } }
+
+        public LimitedSizeArray(int size)
+        {
+            _array = new T[size];
+            Clear();
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (!IsIndexInRange(index))
+                {
+                    return default(T);
+                }
+                return _array[ToArrayIndex(index)];
+            }
+
+            set
+            {
+                if (IsIndexInRange(index))
+                {
+                    _array[ToArrayIndex(index)] = value;
+                }
+            }
+        }
+
+        private bool IsIndexInRange(int index)
+        {
+            return index >= 0 && index >= _firstIndex && index < Count;
+        }
+
+        private int ToArrayIndex(int index)
+        {
+            return (_firstArrayIndex + index) % _array.Length;
+        }
+
+        public void Add(T element)
+        {
+            Count++;
+
+            if (Count > _array.Length)
+            {
+                _firstArrayIndex++;
+            }
+
+            this[Count - 1] = element;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            for (int i = _firstIndex; i < _firstIndex + Count; i++)
+            {
+                result = result + i + " : " + this[i] + "|| ";
+            }
+            return result;
+        }
+
+        [Obsolete("Complex operation!")]
+        public int IndexOf(T item)
+        {
+            int index = 0;
+
+            foreach (T t in this)
+            {
+                if (item.Equals(t))
+                {
+                    return index;
+                }
+                index++;
+            }
+
+            return -1;
+        }
+
+        [Obsolete("Complex operation")]
+        public void Insert(int index, T item)
+        {
+            if (IsIndexInRange(index))
+            {
+                for (int i = index + 1; i < Count; i++)
+                {
+                    this[i - 1] = this[i];
+                }
+                Count--;
+            }
+        }
+
+        [Obsolete("Complex operation")]
+        public void RemoveAt(int index)
+        {
+            if (IsIndexInRange(index))
+            {
+                for (int i = index; i < Count - 1; i++)
+                {
+                    this[i] = this[i + 1];
+                }
+                Count--;
+            }
+        }
+
+        public void Clear()
+        {
+            _firstIndex = 0;
+            Count = 0;
+            _firstArrayIndex = 0;
+        }
+
+        [Obsolete("Complex operation")]
+        public bool Contains(T item)
+        {
+            return IndexOf(item) != -1;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            int count = Mathf.Min(_array.Length, Count);
+            if (array.Length - arrayIndex > count)
+            {
+                Debug.LogError("Array length should not exceed " + count);
+                return;
+            }
+
+            for (int i = _firstIndex; i < Count; i++)
+            {
+                array[i - _firstIndex + arrayIndex] = this[i];
+            }
+        }
+
+        [Obsolete("Complex operation")]
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index == -1)
+            {
+                return false;
+            }
+            RemoveAt(index);
+
+            return true;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+
+        #region IEnumerator
+
+        private int _enumerableIndex;
+
+        public T Current { get { return this[_enumerableIndex]; } }
+
+        object IEnumerator.Current { get { return Current; } }
+
+        public bool MoveNext()
+        {
+            _enumerableIndex++;
+            return _enumerableIndex < Count;
+        }
+
+        public void Reset()
+        {
+            _enumerableIndex = _firstIndex;
+        }
+
+        public void Dispose()
+        {
+            int arraySize = _array.Length;
+            Clear();
+            _array = new T[arraySize];
+        }
+
+        #endregion
+    }
+}
+
+
